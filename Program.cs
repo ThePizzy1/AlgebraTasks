9fbd07d1d9f43a8985d6dd87995c81377e41c88f
ThePizzy1 <ivavozab03@gmail.com>
2026-03-17 20:42:53 +0100

    Commit_17_3_2026_01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Algebra1
+﻿using System;
+namespace Algebra1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //komentari 
+            /*
+             * komentari ctrl+k+c
+             * odkomentiraj ctrl+k+u
+             *  označi strelica+shift
+             * k+f formatiranje
+             *  k+e  ukloni nepotrebne using direktive
+             */
+            string name = "Iva";
+            Console.WriteLine(name);
+            Console.WriteLine("Ovo je " + name);
+            Console.WriteLine('I');
+            Console.WriteLine("Ime je {0}", name);
+
+            //interpolacija stringa
+            Console.BackgroundColor=ConsoleColor.Red;
+            Console.WriteLine($"Po treći put ovo je {name}");
+
+            Console.Write("Pozdrav, ");
+            Console.Write(name);
+            Console.Write("!");
+            Console.ResetColor();
+            int years = 22;
+
+            System.Console.WriteLine("Pogodi tko je!");
+
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.WriteLine($"{name}! I imam {years} godine :)");
+            Console.ResetColor();
+
+            //eksplicitni sting i int
+            //var ne možeš samo deklarirati moraš i inicijalizrati da kompajler zna koji je tip podatka
+            var a = "100";//string
+            var b = 200;//int
+
+
+            //Console.ReadLine();
         }
     }
 }
